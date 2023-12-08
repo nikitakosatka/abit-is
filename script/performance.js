d301a8c5a7c799eb45b0e44c70c8ac9c8ff8dfdb
Nikita Usatov <nikitakosatka@yandex.ru>
2023-12-09 00:07:10 +0300

    feat: performance & change table to grid

---
+++ script/performance.js
@@ -0,0 +1,10 @@
+(function() {
+    const startTime = performance.now();
+
+    window.addEventListener("load", function() {
+        const endTime = performance.now();
+        const loadTime = endTime - startTime;
+        const loadTimeElement = document.getElementById("loadTime");
+        loadTimeElement.textContent = Math.round(loadTime);
+    });
+})();
