6f71aefe6228f2c81cfafa0fd577cf40d112d7ca
Nikita Usatov <nikitakosatka@yandex.ru>
2023-12-09 17:33:31 +0300

    feat: labs 4-5

---
+++ script/study_plan.js
@@ -0,0 +1,13 @@
+function showDescription(subjectId) {
+    const description = document.getElementById(subjectId);
+
+    if (description.style.display === 'block') {
+        description.style.display = 'none';
+    } else {
+        const descriptions = document.getElementsByClassName('description');
+        for (let i = 0; i < descriptions.length; i++) {
+            descriptions[i].style.display = 'none';
+        }
+        description.style.display = 'block';
+    }
+}
