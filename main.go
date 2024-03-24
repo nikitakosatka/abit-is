599975ba999db422bb9266e865dc2bfae29acbbb
Nikita Usatov <nikitakosatka@yandex.ru>
2024-03-24 22:00:49 +0300

    Return static

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"net/http"
 )
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	r.Handle("/*", http.StripPrefix("/", fileServer))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		http.ServeFile(w, r, "./static/index.html")
 	})
 	http.ListenAndServe(":8080", r)
 }
