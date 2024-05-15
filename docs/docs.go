08e41efcd8bd721b8d63a0f032061ba495e848f3
Nikita Usatov <nikitakosatka@yandex.ru>
2024-05-16 02:22:50 +0300

    Regenerate docs

--- docs/docs.go
+++ docs/docs.go
@@ -912,7 +912,7 @@ var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
 	Host:             "",
 	BasePath:         "/",
-	Schemes:          []string{"http"},
+	Schemes:          []string{"https"},
 	Title:            "Abit IS",
 	Description:      "Abit service.",
 	InfoInstanceName: "swagger",
